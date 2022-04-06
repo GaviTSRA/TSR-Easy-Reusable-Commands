621638a063decf29236fad21f90734218149049d
GaviTSRA <gavitsra@gmail.com>
2022-04-06 17:20:50 +0200

    Added settings

--- scripts/main.js
+++ scripts/main.js
@@ -9,53 +9,62 @@ function loadDialog() {
 	const a = table.table().center().bottom().get();
 
 	var cmd1, cmd2, cmd3, cmd4;
-	a.field("", n=>{cmd1 = n}).width(500).get();
+	a.field(Core.settings.get("cmd1", ""), n=>{cmd1 = n; Core.settings.put("cmd1", n)}).width(500).get();
 
 	a.button("Execute", Icon.warning, () => {
 		Call.sendChatMessage(cmd1);
-		dialog.hide();
+		if(Core.settings.get("auto-exit", true))
+			dialog.hide();
 	}).width(200).height(75);
 	
 	a.add("     ");
 	a.row();
 
 
-	a.field("", n=>{cmd2 = n}).width(500).get();
+	a.field(Core.settings.get("cmd2", ""), n=>{cmd2 = n; Core.settings.put("cmd2", n)}).width(500).get();
 
 	a.button("Execute", Icon.warning, () => {
 		Call.sendChatMessage(cmd2);
-		dialog.hide();
+		if(Core.settings.get("auto-exit", true))
+			dialog.hide();
 	}).width(200).height(75);
 	
 	a.add("     ");
 	a.row();
 
 
-	a.field("", n=>{cmd3 = n}).width(500).get();
+	a.field(Core.settings.get("cmd3", ""), n=>{cmd3 = n; Core.settings.put("cmd3", n)}).width(500).get();
 
 	a.button("Execute", Icon.warning, () => {
 		Call.sendChatMessage(cmd3);
-		dialog.hide();
+		if(Core.settings.get("auto-exit", true))
+			dialog.hide();
 	}).width(200).height(75);
 	
 	a.add("     ");
 	a.row();
 
 
-	a.field("", n=>{cmd4 = n}).width(500).get();
+	a.field(Core.settings.get("cmd4", ""), n=>{cmd4 = n; Core.settings.put("cmd4", n)}).width(500).get();
 
 	a.button("Execute", Icon.warning, () => {
 		Call.sendChatMessage(cmd4);
-		dialog.hide();
+		if(Core.settings.get("auto-exit", true))
+			dialog.hide();
 	}).width(200).height(75);
 	
 	a.add("     ");
 	a.row();
 
 	dialog.addCloseButton();
+        dialog.buttons.button("Reload settings", Icon.logic, () => {
+		loadDialog(); 
+	});
 };
 
 ui.onLoad(() => {
+        Vars.ui.settings.game.checkPref("save-commands", Core.settings.getBool("save-commands", true));
+        Vars.ui.settings.game.checkPref("auto-exit", Core.settings.getBool("auto-exit", false));
 	loadDialog();
 });
 
